4eeba22d49145b1f32e6f11614108b8ffcdf08c8
PathFinder <pathfinder@PathFinder.local>
2020-11-10 19:56:13 +0000

    upload origins

---
+++ sync.sh
@@ -0,0 +1,3 @@
+rsync -vazh cam:/mnt/DATA_OTHER/breakHis/ /mnt/DATA_OTHER/breakHis/ 
+rsync -vazh /mnt/DATA_OTHER/digestPath/ cam:/mnt/DATA_OTHER/digestPath/
+rsync -vazh /mnt/DATA_OTHER/moNuSeg/ cam:/mnt/DATA_OTHER/moNuSeg/
